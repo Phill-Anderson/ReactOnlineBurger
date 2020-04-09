bfdf43ee79b5223afa5abe353c771ba8fa510bab
Phill Anderson <eaglesquad86@gmail.com>
2020-04-09 18:42:47 +0800

    Map функцын index аргумэнтийг ашиглан key давхардахаас сэргийлсэн нь

--- src/components/Comment/index.js
+++ src/components/Comment/index.js
@@ -1,32 +1,46 @@
 import React, { Component } from "react";
-import {MyCoolButton} from "../MyCoolButton";
+import { MyCoolButton } from "../MyCoolButton";
 import "./style.css";
 import App from "../App";
 // класс компонэнт
 class Comment extends Component {
-    // класс компонэнт нь өөртөө state хадгалах зорилготойгоор ашиглагдаж эхэлсэн бөгөөд энэ нь ихээн хэмжээтэй том төслүүд дээр асуудлыг үүсгэдэг
-    constructor(){
-        super();
-        this.state = {
-            publisher: '',
-            comment: ''
-        };
-    }
-    bichih = (e)=>{
-       this.setState({ comment: e.target.value });
-    }
-    render(){
-        return( 
-       <div>
-            <div>{ this.state.comment }</div>
-            <div className="CoolText">Зохиогч: { this.props.zohiogch }</div>
-            <textarea onChange={this.bichih}></textarea>
-            <MyCoolButton comment = {this.state.comment} tovchNer = "Коммэнт бич" text="Хадгал"/>  
-              
-       </div>     
-       );
-   
-    }
+  // класс компонэнт нь өөртөө state хадгалах зорилготойгоор ашиглагдаж эхэлсэн бөгөөд энэ нь ихээн хэмжээтэй том төслүүд дээр асуудлыг үүсгэдэг
+  constructor(props) {
+    super(props);
+    console.log(this.props.zohiogch);
+    this.state = {
+      publisher: "",
+      comment: "",
+    };
+  }
+  // constructor функц үүсгэхгүйгээр шууд доорхи байдлаар
+  // state бичиж болно. Энэ тохиолдолд React нь автоматаар constructor функцыг үүсгэж өгдөг
+  /*  state = {
+    publisher: "",
+    comment: "",
+  }; */
+  bichih = (e) => {
+    this.setState({ comment: e.target.value });
+  };
+  render() {
+    return (
+      <div className="CommentContainer">
+        <div className="CoolText">{this.state.comment}</div>
+        <div className="CoolText">Зохиогч: {this.props.zohiogch}</div>
+        <textarea onChange={this.bichih}></textarea>
+        <MyCoolButton
+          comment={this.state.comment}
+          tovchNer="Коммэнт бич"
+          text="Хадгал"
+        />
+        <MyCoolButton
+          comment={this.state.comment}
+          tovchNer="Коммэнт Ил тодоор бичээрэй"
+          text="Хадгалаарай"
+        />
+      </div>
+    );
+  }
 }
 // MyCoolButton дээр бичигдсэн comment нь эх компонэнт руу илгээгдэх ба эх компонэнтээс MyCoolButton нь өөрийн props - оор дамжуулан comment утгыг хүлээн авна
 export default Comment;
--- src/components/MyCoolButton/index.js
+++ src/components/MyCoolButton/index.js
@@ -3,29 +3,41 @@ import "./style.css";
 // функц компонэнт
 // функц компонэнт нь дотроо state байхгүй бөгөөд state хийхийн тулд react hooks зэрэг state management санг ашигладаг бөгөөд энэ нь бүх state - үүдийг
 // нэг дор бөөгнөрүүлж кодыг эмх цэгцтэй болгож өгдөг бөгөөд сүүлийн үеийн react чиг хандлага бол функц компонэнтуудыг хэрэглэх явдал юм.
-export const MyCoolButton = props => {
-    const [commentHistory, setCommentHistory] = useState([]);
-    const  [count, setCount] = useState(0);
-    
-    const clickHandler = () =>{
-        setCount( count + 1 );
-        setCommentHistory([...commentHistory, props.comment]);  // set state -ийг утгаар хангаж байх ёстой 
-       // alert(`Коммэнт бичигдлээ: ${ props.comment }`);
-    }
-   // css.color = "green";
-    return (
-      <div className="MyButton coolText">
-        <span style={css}>{props.tovchNer? props.tovchNer : "Гоёмсог товч"}</span><br/>
-        <input className="MyCoolButton" type="button" value={ props.text ? `${count} ${props.text}` : "Клик" } onClick={clickHandler} />
-        <div>
-            { commentHistory.map( el=>( <div> {el} </div> ) )}
-        </div>
+export const MyCoolButton = (props) => {
+  const [commentHistory, setCommentHistory] = useState([]);
+  const [count, setCount] = useState(0);
 
+  const clickHandler = () => {
+    setCount(count + 1);
+    setCommentHistory([...commentHistory, props.comment]); // set state -ийг утгаар хангаж байх ёстой
+    // alert(`Коммэнт бичигдлээ: ${ props.comment }`);
+  };
+  // css.color = "green";
+  return (
+    <div className="CoolButtonContainer CoolText">
+      <span style={css}>
+        {props.tovchNer ? props.tovchNer : "Гоёмсог товч"}
+      </span>
+      <br />
+      <input
+        className="MyCoolButton"
+        type="button"
+        value={props.text ? `${count} ${props.text}` : "Клик"}
+        onClick={clickHandler}
+      />
+      <div>
+        {commentHistory.map((el, index) => (
+          <div key={index}>
+            {" "}
+            {el} - {index}{" "}
+          </div>
+        ))}
       </div>
-    );
-  };
+    </div>
+  );
+};
 // react inline style бичих арга
-  const css = {
-      backgorundColor: "gray",
-      color : "blue"
-  };
+const css = {
+  backgorundColor: "gray",
+  color: "blue",
+};
